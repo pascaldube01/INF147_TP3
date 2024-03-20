e7a5cf7853795df83351359033eacba2fbcf33d3
SimonDesalliers <simon.des-alliers.1@ens.etsmtl.ca>
2024-03-20 07:36:56 -0400

    Commentaires de prototypes pour liste_coups.hpp

--- liste_coup.hpp
+++ liste_coup.hpp
@@ -78,75 +78,199 @@ typedef struct
 /*-------------------------------------------------------------------*/
 /*                     FONCTIONS INFORMATRICES                       */
 /*-------------------------------------------------------------------*/
-/* obtenir le coup du neoud pointé par le pointeur-courant */
+
+/************************************************************************************************
+    OBJECTIF :  Obtenir le coup du neoud pointé par le pointeur-courant
+
+    PARAMETRES : la liste chaînée de coups 
+
+    SORTIES :	le coup du neoud pointé par le pointeur-courant 
+
+    écrit par Pascal Dubé, Victor Poulin et Simon Des-Alliers
+***********************************************************************************************/
 t_coup get_coup_pc(const t_liste_coups* liste_coups);
 
-/* Accesseur qui permet de savoir le nombre de coups valides dans la liste. */
+/************************************************************************************************
+    OBJECTIF :  Accesseur qui permet de savoir le nombre de coups valides dans la liste.
+
+    PARAMETRES : la liste chaînée de coups
+
+    SORTIES :	le nombre de coups valides (nb_noeud)
+
+    écrit par Pascal Dubé, Victor Poulin et Simon Des-Alliers
+***********************************************************************************************/
 int get_nb_coups(const t_liste_coups* liste_coups);
 
-/* Valide si le "coup" reçu est dans la liste des coups possibles à jouer.  On reçoit  */
-/* la représentation-texte du coup à retrouver.  On parcourt la liste en comparant le  */
-/* texte reçu avec le champ "texte_coup" de chaque coup.  Si ce texte a été retrouvé,  */
-/* on retourne une copie de ce coup via la référence "*coup" et on retourne 1.  Sinon, */
-/* on retourne 0 (coup non-présent dans la liste). */
+/************************************************************************************************
+    OBJECTIF : Valide si le "coup" reçu est dans la liste des coups possibles à jouer. On
+               reçoit la représentation-texte du coup à retrouver.  On parcourt la liste en 
+               comparant le texte reçu avec le champ "texte_coup" de chaque coup.  Si ce texte
+               a été retrouvé, on retourne une copie de ce coup via la référence "*coup" et on 
+               retourne 1. Sinon, on retourne 0 (coup non-présent dans la liste).
+
+    PARAMETRES : la liste chaînée de coups, le texte_coup à comparer et le coup à copier
+
+    SORTIES :	Retourne 1 si texte_coup trouvé, 0 sinon.
+
+    écrit par Pascal Dubé, Victor Poulin et Simon Des-Alliers
+***********************************************************************************************/
 int valider_coup(t_liste_coups* liste_coups, char* texte_coup, t_coup* coup);
 
-/* Valide si la position-destination (col, lig) reçue fait partie d'un des coups de la */
-/* liste. On reçoit les coordonnées de la case "col, lig" et on parcourt la liste à la */
-/* recherche d'un coup qui a la même case-destination que (col, lig). Dès qu'on trouve */
-/* un coup qui atteint cette case, la fonction arrête sa recherche et retourne 1.      */
-/* Sinon, elle retourne 0 (aucun coup de la liste se rends à la case [col, lig]). */
+/************************************************************************************************
+    OBJECTIF : Valide si la position-destination (col, lig) reçue fait partie d'un des coups de la 
+               liste. On reçoit les coordonnées de la case "col, lig" et on parcourt la liste à la 
+               recherche d'un coup qui a la même case-destination que (col, lig). Dès qu'on trouve 
+               un coup qui atteint cette case, la fonction arrête sa recherche et retourne 1.      
+               Sinon, elle retourne 0 (aucun coup de la liste se rends à la case [col, lig]). 
+
+    PARAMETRES : la liste chaînée de coups, la colonne et ligne et destination.
+
+    SORTIES :	Si on trouve la case, on retourne 1, 0 sinon.
+
+    écrit par Pascal Dubé, Victor Poulin et Simon Des-Alliers
+***********************************************************************************************/
 int valider_case_dest(t_liste_coups* liste_coups, int col, int lig);
 
 /*-------------------------------------------------------------------*/
 /*                         LES MUTATEURS                             */
 /*-------------------------------------------------------------------*/
-/* Initialiser la liste de coups. On mets les trois pointeurs de la liste à NULL et on */
-/* met le nombre d'éléments de la liste à zéro. */
+
+/************************************************************************************************
+    OBJECTIF : Initialiser la liste de coups. On mets les trois pointeurs de la liste à NULL et on 
+               met le nombre d'éléments de la liste à zéro. 
+
+    PARAMETRES : la liste chaînée de coups.
+
+    SORTIES :	Ne retourne rien (void)
+
+    écrit par Pascal Dubé, Victor Poulin et Simon Des-Alliers
+***********************************************************************************************/
 void init_liste_coups(t_liste_coups* liste_coups);
 
-/* Fonction qui crée un nouveau coup. Les 6 derniers paramètres sont copiés dans leurs */
-/* champs respectifs de la référence "coup".  On doit aussi former la représentation-  */
-/* texte du coup à partir des valeurs "col, lig" et "col_dest, lig_dest". Eg. "a2-a4". */
+/************************************************************************************************
+    OBJECTIF : Fonction qui crée un nouveau coup. Les 6 derniers paramètres sont copiés dans leurs 
+               champs respectifs de la référence "coup".  On doit aussi former la représentation-  
+               texte du coup à partir des valeurs "col, lig" et "col_dest, lig_dest". Eg. "a2-a4". 
+
+    PARAMETRES : Le coup a créer. La colonne, la ligne, la colonne destination, la ligne
+                 destination, la colonne 2  et la ligne 2 à copier.
+
+    SORTIES :	Ne retourne rien (void)
+
+    écrit par Pascal Dubé, Victor Poulin et Simon Des-Alliers
+***********************************************************************************************/
 void set_coup(t_coup* coup, int col, int lig, int col_dest, int lig_dest, int col2, int lig2);
 
-/* Ajoute un nouveau coup à la fin de la liste de coups reçue en référence. On crée un */
-/* nouveau noeud. On y copie le coup reçu et on l'ajoute à la FIN de la liste chainée. */
-/* Si l'allocation du nouveau noeud a échoué, la fonction retourne 0. Sinon, on ajoute */
-/* le coup, on place le pointeur de fin de liste sur le nouveau coup et on retourne 1. */
+/************************************************************************************************
+    OBJECTIF : Ajoute un nouveau coup à la fin de la liste de coups reçue en référence. On crée un 
+               nouveau noeud. On y copie le coup reçu et on l'ajoute à la FIN de la liste chainée. 
+               Si l'allocation du nouveau noeud a échoué, la fonction retourne 0. Sinon, on ajoute 
+               le coup, on place le pointeur de fin de liste sur le nouveau coup et on retourne 1. 
+
+    PARAMETRES : La liste chaînée de coups et le coup à ajouter.
+
+    SORTIES :	Retourne 1 si le coup a été ajouté et l'allocation s'est bien passée, 0 sinon.
+
+    écrit par Pascal Dubé, Victor Poulin et Simon Des-Alliers
+***********************************************************************************************/
 int ajouter_coup(t_liste_coups* liste_coups, const t_coup* coup);
 
-/* Ajoute un coup au début de la liste de coups. On crée un nouveau noeud. On y copie  */
-/* le coup reçu et on l'ajoute au DÉBUT de la liste. Si l'allocation du nouveau noeud  */
-/* a échoué, la fonction retourne 0. Sinon, on ajoute le coup et on retourne 1. */
+/************************************************************************************************
+    OBJECTIF :  Ajoute un coup au début de la liste de coups. On crée un nouveau noeud. On y copie  
+                le coup reçu et on l'ajoute au DÉBUT de la liste. Si l'allocation du nouveau noeud  
+                a échoué, la fonction retourne 0. Sinon, on ajoute le coup et on retourne 1. 
+
+    PARAMETRES : La liste chaînée de coups et le coup à ajouter.
+
+    SORTIES :	Retourne 1 si le coup a été ajouté et l'allocation s'est bien passée, 0 sinon.
+
+    écrit par Pascal Dubé, Victor Poulin et Simon Des-Alliers
+***********************************************************************************************/
 int ajouter_coup_debut(t_liste_coups* liste_coups, const t_coup* coup);
 
-/* Vider la liste des coups en détruisant tous les noeuds de la liste et en remettant  */
-/* son "nb_noeuds" à zéro. Les trois pointeurs seront mis à NULL. */
+/************************************************************************************************
+    OBJECTIF :   Vider la liste des coups en détruisant tous les noeuds de la liste et en remettant  
+                 son "nb_noeuds" à zéro. Les trois pointeurs seront mis à NULL. 
+
+    PARAMETRES : La liste chaînée de coups
+
+    SORTIES :	Ne retourne rien (void)
+
+    écrit par Pascal Dubé, Victor Poulin et Simon Des-Alliers
+***********************************************************************************************/
 void vider_liste_coups(t_liste_coups* liste_coups);
 
-/* Replacer le pointeur-courant sur le noeud de tête. */
+/************************************************************************************************
+    OBJECTIF :  Replacer le pointeur-courant sur le noeud de tête. 
+
+    PARAMETRES : La liste chaînée de coups
+
+    SORTIES :	Ne retourne rien (void)
+
+    écrit par Pascal Dubé, Victor Poulin et Simon Des-Alliers
+***********************************************************************************************/
 void replacer_pc_debut(t_liste_coups* liste_coups);
 
-/* Si le pointeur-courant est non-NULL et qu'il ne pointe pas sur le dernier noeud, on */
-/* l'avance au noeud suivant et on retourne 1. Sinon on le bouge pas et on retourne 0. */
+/************************************************************************************************
+    OBJECTIF :   Si le pointeur-courant est non-NULL et qu'il ne pointe pas sur le dernier noeud,
+                 on l'avance au noeud suivant et on retourne 1. Sinon on le bouge pas et on 
+                 retourne 0. 
+
+    PARAMETRES : La liste chaînée de coups
+
+    SORTIES :	Retourne 1 si on avance le pointeur courant, 0 sinon.
+
+    écrit par Pascal Dubé, Victor Poulin et Simon Des-Alliers
+***********************************************************************************************/
 int  avancer_pc(t_liste_coups* liste_coups);
 
-/* Effectue un choix de coup aléatoire dans la liste de coups. On génére une position  */
-/* aléatoirement entre [0..nb_noeuds-1] et on parcours la liste à partir du début en   */
-/* faisant "pos_alea" déplacements (avec le "p_courant").  On retourne le coup qui se  */
-/* trouve dans le noeud suite aux "pos_alea" déplacements (utilisée pour le MANDAT 1). */
+/************************************************************************************************
+    OBJECTIF :   Effectue un choix de coup aléatoire dans la liste de coups. On génére une position  
+                 aléatoirement entre [0..nb_noeuds-1] et on parcours la liste à partir du début en   
+                 faisant "pos_alea" déplacements (avec le "p_courant").  On retourne le coup qui se  
+                 trouve dans le noeud suite aux "pos_alea" déplacements (utilisée pour le MANDAT 1) 
+
+    PARAMETRES : La liste chaînée de coups
+
+    SORTIES :	Renvoie le coup choisit aléatoirement
+
+    écrit par Pascal Dubé, Victor Poulin et Simon Des-Alliers
+***********************************************************************************************/
 t_coup choix_coup_ordi(t_liste_coups* liste_coups);
 
-/* Affiche le contenu de la liste dans la console-texte (SANS le "Winconsole" pour la  */
-/* version finale du projet).  Sera utile pour les débuggages seulement. */
+/************************************************************************************************
+    OBJECTIF :   Affiche le contenu de la liste dans la console-texte (SANS le "Winconsole" pour 
+                 la version finale du projet). Sera utile pour les débuggages seulement.
+
+    PARAMETRES : La liste chaînée de coups
+
+    SORTIES :	Ne retourne rien (void)
+
+    écrit par Pascal Dubé, Victor Poulin et Simon Des-Alliers
+***********************************************************************************************/ 
 void afficher_liste_coups(t_liste_coups* liste_coups);
 
-/* Détruire la liste de coups. Faites simplement un appel à "vider_liste_coups()" ici. */
+/************************************************************************************************
+    OBJECTIF : Détruire la liste de coups.
+
+    PARAMETRES : La liste chaînée de coups
+
+    SORTIES :	Ne retourne rien (void)
+
+    écrit par Pascal Dubé, Victor Poulin et Simon Des-Alliers
+***********************************************************************************************/
 void detruire_liste_coups(t_liste_coups* liste_coups);
 
-//Renvoie 1 si la liste est vide, 0 sinon
-int liste_est_vide(t_liste_coups* liste);
+/************************************************************************************************
+    OBJECTIF : Permet de savoir si la liste contient des éléments ou non
+
+    PARAMETRES : La liste chaînée de coups
+
+    SORTIES :	Retourne 1 si la liste est vide, 0 sinon.
+
+    écrit par Pascal Dubé, Victor Poulin et Simon Des-Alliers
+***********************************************************************************************/
+int liste_est_vide(t_liste_coups* liste_coups);
 
 
 #endif
