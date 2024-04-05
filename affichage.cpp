248cff34ccf0a1ed34d062965b69df39b03b78a8
victorpoulin123 <victor.poulin.1@ens.etsmtl.ca>
2024-04-04 22:40:07 -0400

    afficher gagnant
    message de victoire en haut du message de fin

--- affichage.cpp
+++ affichage.cpp
@@ -240,14 +240,14 @@ void afficher_gagnant(int joueur)
 	/*Si le joueur est blanc*/
 	if (joueur)
 	{
-		afficher_message("Le gagnant est le joueur blanc!");
-		delai_ecran(1500);
+		afficher_info("Le gagnant est le joueur blanc!", joueur);
+		delai_ecran(500);
 	}
 	else
 	{
 		/*Si le joueur est noir*/
-		afficher_message("Le gagnant est le joueur noir!");
-		delai_ecran(1500);
+		afficher_info("Le gagnant est le joueur noir!", joueur);
+		delai_ecran(500);
 	}
 		
 }
